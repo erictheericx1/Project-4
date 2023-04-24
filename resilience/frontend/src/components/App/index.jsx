45304c313a46d4e042b1628899a3c20ab2c32bde
erictheericx1 <e.lopez.7.el@gmail.com>
2023-04-24 14:18:23 -0700

    API working

--- resilience/frontend/src/components/App/index.jsx
+++ resilience/frontend/src/components/App/index.jsx
@@ -3,16 +3,23 @@ import { useState, useEffect } from 'react'
 import axios from 'axios';
 
 function App() {
-  const [exercises] = useState([])
+  const [exercises, setExercises] = useState([])
 
   // Query the API
   useEffect(() => {
 
     async function getData() {
-      const res = await axios.get('https://exercisedb.p.rapidapi.com/exercises/bodyPart')
-      return res.data;
-      // setExercises(data)
+      const response = await axios.get('https://exercisedb.p.rapidapi.com/exercises/bodyPart/back', {
+        headers: {
+          'content-type': 'application/octet-stream',
+          'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+          'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        }
+      })
+      setExercises(response.data)  
+  
     }
+
 
     //call the async function
     getData()
@@ -23,7 +30,7 @@ function App() {
   return (
     <>
       <h1>Resilience</h1>
-      {exercises.length > 0 ? <img src={exercises[0].image} /> : <p>Please wait while we load your exercise...</p>} 
+      {exercises.length > 0 ? <img src={exercises[0].gifUrl} /> : <p>Please wait while we load your exercise...</p>} 
 
     </>
   )
